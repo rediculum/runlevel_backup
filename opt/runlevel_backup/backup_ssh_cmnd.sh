efd754d2a53d66c393d770b3e6b3be9fe191dd68
ReDiculum <rediculum0@runlevel.ch>
2026-05-04 09:49:11 +0200

    :arrow_up: upgrade rdiff 2.0 command

--- opt/runlevel_backup/backup_ssh_cmnd.sh
+++ opt/runlevel_backup/backup_ssh_cmnd.sh
@@ -1,6 +1,6 @@
 case "$SSH_ORIGINAL_COMMAND" in
   "test -d /"* |\
-  "rdiff-backup --server" |\
+  "rdiff-backup server" |\
   "id" \
   )
     logger -p auth.info -t backup_sshcmd "$LOGNAME executed: $SSH_ORIGINAL_COMMAND"
